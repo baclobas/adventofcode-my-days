e21dd670aaa530d11843abe81fcab23a2d77a3c6
Andrei Kuspitz <akuspitz@gmail.com>
2025-04-09 09:36:14 +0000

    Day 1 part 2 puzzle solved

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day_1_solution() {
+    fn day1_part1_total_distances() {
         let mut left_vec = Vec::<i32>::new();
         let mut right_vec = Vec::<i32>::new();
         populate_lists("./input.txt", &mut left_vec, &mut right_vec);
@@ -31,5 +31,30 @@ mod tests {
         let dists: Vec<i32> = pairs.iter().map(|e| (e.1 - e.0).abs()).collect();
         println!("{:?}", dists);
         assert_eq!(1941353, dists.iter().sum());
+    }
+
+    #[test]
+    fn day1_part2_similarity_index() {
+        use std::collections::HashMap;
+        let mut left_vec = Vec::<i32>::new();
+        let mut right_vec = Vec::<i32>::new();
+        populate_lists("./input.txt", &mut left_vec, &mut right_vec);
+        let mut scores: HashMap<i32, i32> = HashMap::new();
+        for li in &left_vec {
+            scores.insert(*li, 0);
+            for ri in &right_vec {
+                if *li == *ri {
+                    scores.insert(*li, 1 + scores[li]);
+                } else {
+                    continue;
+                };
+            }
+        }
+        println!("{:?}", scores);
+        let mut sum_of_scores = 0;
+        for (k, v) in &scores {
+            sum_of_scores += *k * *v;
+        }
+        assert_eq!(22539317, sum_of_scores);
     }
 }
